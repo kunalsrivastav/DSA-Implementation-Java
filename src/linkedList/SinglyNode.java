9cf039b3a58ba58ec52fb31b662acf02d4911b9a
Kunal Srivastav <sri2001kunal@gmail.com>
2021-03-31 18:44:55 +0530

    SinglyNode.java completed.

---
+++ src/linkedList/SinglyNode.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 31-03-2021
+ *   Time: 18:44
+ *   File: SinglyNode.java
+ */
+
+package linkedList;
+
+public class SinglyNode {
+    int data;
+    SinglyNode next;
+
+    public SinglyNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
