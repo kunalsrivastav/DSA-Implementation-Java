81a4ec816a4c2345bb50bb830b352e49f19fdbe2
Kunal Srivastav <sri2001kunal@gmail.com>
2021-03-31 18:51:42 +0530

    Implemented Node class in SinglyLinkedList.java

---
+++ src/linkedList/SinglyLinkedList.java
@@ -0,0 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 31-03-2021
+ *   Time: 18:50
+ *   File: SinglyLinkedList.java
+ */
+
+package linkedList;
+
+public class SinglyLinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
